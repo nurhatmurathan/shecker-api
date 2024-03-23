61d2de80d2cbf259d9f94c476b28e9f6871f7d3e
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-03-23 12:23:18 +0600

    init: models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,48 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Fridge(models.Model):
+    account = models.CharField(primary_key=True)
+    description = models.TextField(null=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+    price = models.IntegerField()
+    image = models.URLField(null=True)
+
+
+class FridgeProducts(models.Model):
+    fridge = models.ForeignKey(Fridge, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "pending"
+        CHECKED = "CHECKED", "checked"
+        PAYED = "PAYED", "payed"
+        FAILED = "FAILED", "failed"
+
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class OrderProducts(models.Model):
+    fridge_products = models.ForeignKey(FridgeProducts, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    amount = models.IntegerField()
+
+
+class Transaction(models.Model):
+    prv_txn_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    check_txn_id = models.UUIDField(null=True)
+    pay_txn_id = models.UUIDField(null=True)
+    txn_date = models.CharField(null=True)
+
+
