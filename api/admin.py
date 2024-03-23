ef0e298cf82f3037ba5daf1b083a9f7137ac0ec7
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-03-23 13:26:05 +0600

    feat: django admin

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,57 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+
 from .models import *
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', "price")
+    list_display_links = ('id',)
+
+
+class FridgeProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fridge_link', "product_link", "quantity")
+    list_display_links = ('id',)
+
+    def fridge_link(self, obj):
+        url = reverse("admin:api_fridge_change", args=[obj.fridge.account])
+        return format_html('<a href="{}">{}</a>', url, obj.fridge)
+
+    fridge_link.short_description = 'Fridge'
+
+    def product_link(self, obj):
+        url = reverse("admin:api_product_change", args=[obj.product.id])
+        return format_html('<a href="{}">{}</a>', url, obj.product)
+
+    product_link.short_description = 'Product'
+
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fridge_product_link', "order_link", "amount")
+    list_display_links = ('id',)
+
+    def fridge_product_link(self, obj):
+        url = reverse("admin:api_fridgeproduct_change", args=[obj.fridge_product.id])
+        return format_html('<a href="{}">{}</a>', url, obj.fridge_product)
+
+    fridge_product_link.short_description = 'Fridge Product'
+
+    def order_link(self, obj):
+        url = reverse("admin:api_order_change", args=[obj.order.id])
+        return format_html('<a href="{}">{}</a>', url, obj.order)
+
+    order_link.short_description = 'Order'
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('prv_txn_id', "order", "check_txn_id", "pay_txn_id", "txn_date")
+    list_display_links = ('prv_txn_id',)
+
+
 admin.site.register(Fridge)
-admin.site.register(Product)
-admin.site.register(FridgeProduct)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(FridgeProduct, FridgeProductAdmin)
 admin.site.register(Order)
-admin.site.register(OrderProduct)
-admin.site.register(Transaction)
+admin.site.register(OrderProduct, OrderProductAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- api/models.py
+++ api/models.py
@@ -5,20 +5,32 @@ from django.db import models
 
 class Fridge(models.Model):
     account = models.CharField(primary_key=True)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.account
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     price = models.IntegerField()
-    image = models.URLField(null=True)
+    image = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class FridgeProduct(models.Model):
     fridge = models.ForeignKey(Fridge, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.IntegerField()
+
+    class Meta:
+        unique_together = [['fridge', 'product']]
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Order(models.Model):
@@ -31,18 +43,30 @@ class Order(models.Model):
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
     date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.id)
+
 
 class OrderProduct(models.Model):
-    fridge_products = models.ForeignKey(FridgeProduct, on_delete=models.PROTECT)
+    fridge_product = models.ForeignKey(FridgeProduct, on_delete=models.PROTECT)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     amount = models.IntegerField()
+
+    class Meta:
+        unique_together = [['fridge_product', 'order']]
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Transaction(models.Model):
     prv_txn_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    check_txn_id = models.UUIDField(null=True)
-    pay_txn_id = models.UUIDField(null=True)
-    txn_date = models.CharField(null=True)
+    check_txn_id = models.UUIDField(null=True, blank=True)
+    pay_txn_id = models.UUIDField(null=True, blank=True)
+    txn_date = models.CharField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.prv_txn_id)
 
 
