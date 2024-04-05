1847a1b7bb1f0d7bdf54d2c7cd2c3fa3fd7ba621
AferDust <aferdust27@gmail.com>
2024-04-05 15:10:53 +0500

    fix: .env variable name changing

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ load_dotenv('.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 # .envs
 SECRET_KEY = os.getenv("SECRET_KEY")
-BIN = os.getenv("KASPI_BIN")
+BIN = os.getenv("BIN")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
