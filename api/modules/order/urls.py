19bb5ddb97d3d7ea981b81b7053c5e0b7f17c7cf
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-04-16 15:51:27 +0500

    order detail

--- api/modules/order/urls.py
+++ api/modules/order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from api.modules.order.views import OrderAPIView
+from api.modules.order.views import OrderAPIView, OrderDetailView
 
 urlpatterns = [
-    path('create/', OrderAPIView.as_view(), name='create-order')
+    path('create/', OrderAPIView.as_view(), name='create-order'),
+    path('detail/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
 ]
