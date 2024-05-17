a96d86062fc59302f5064dcc237a9de172453c65
nurhatmurathan <nurhatmurathan001@gmail.com>
2024-05-17 12:11:47 +0500

    fix: config

--- api/modules/order/views.py
+++ api/modules/order/views.py
@@ -1,5 +1,4 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
